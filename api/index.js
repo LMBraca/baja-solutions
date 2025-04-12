b1448b592a90b178189ba1c1264fde69eb1c151b
LMBraca <102985211+LMBraca@users.noreply.github.com>
2025-04-12 01:16:10 -0700

    render prep

--- api/index.js
+++ api/index.js
@@ -4,10 +4,9 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import cookieParser from "cookie-parser";
+import path from "path";
 
-// If you are using ES modules, you'll need the __dirname workaround:
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 // Import your routes
 import UserRoutes from "./routes/user.route.js";
@@ -35,15 +34,12 @@ app.use("/api/messages", MessageRoutes);
 app.use("/api/cities", CityRoutes);
 app.use("/api/categories", CategoryRoutes);
 
-// 1) Serve your built React files from the client/dist folder
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-// 2) For any route that doesn't match /api, serve index.html so React can handle routing
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
-// Custom error handler (if you need it)
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
@@ -54,8 +50,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-// IMPORTANT: Use process.env.PORT, especially on Render
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
